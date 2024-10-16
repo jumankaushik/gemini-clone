2314216ac0e5e10ba8940248782b5b4b3aaa6469
Juman Kaushik <juman.code@gmail.com>
2024-10-16 23:37:23 +0530

    ui completed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import './Main.css'
+import { assets } from '../../assets/assets'
 
 const Main = () => {
   return (
     <div className='main'>
-        
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Dev.</span></p>
+                <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beatiful places to visit in India</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Briefly summerize this concept: Meme marketing</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Brainstrom ideas about how to impress my HR</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>How to center a div?</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                    Gemini can make mistakes, so double-check it
+                </p>
+            </div>
+        </div>
     </div>
   )
 }
